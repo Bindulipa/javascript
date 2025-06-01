0cfec5cf320d3f38d114fd5abb0c69ba4880b404
Bindulipa <bindulipas@gmail.com>
2025-06-01 13:30:07 +0000

    variable

---
+++ .devcontainer/01_basic/01_Variables.js
@@ -0,0 +1,16 @@
+const accountID = 14435;
+let accountEmail = "bindu@gmail.com";
+var accountPwd = "1234";
+accountCity = "Pune";
+
+//accountID = 2;   [not allowed]
+accountEmail = "ghjkl@gmail.com";
+accountPwd = "345678";
+accountCity ="Bbsr";
+
+
+//console.log(accountID);
+console.table([accountID,accountEmail,accountPwd,accountCity]);
+
+
+
