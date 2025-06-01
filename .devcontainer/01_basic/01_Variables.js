9b9406cc456965550e617511f5f2b5b35a08b9f9
Bindulipa <bindulipas@gmail.com>
2025-06-01 14:53:40 +0000

    .

--- .devcontainer/01_basic/01_Variables.js
+++ .devcontainer/01_basic/01_Variables.js
@@ -2,15 +2,16 @@ const accountID = 14435;
 let accountEmail = "bindu@gmail.com";
 var accountPwd = "1234";
 accountCity = "Pune";
+let accountState;  //if you dont give any value after declaring a variable then output will be undefined .
 
 //accountID = 2;   [not allowed]
 accountEmail = "ghjkl@gmail.com";
 accountPwd = "345678";
 accountCity ="Bbsr";
-
+/*dont use VAR for variable declaration because of issue in block scope and functional scope  */
 
 //console.log(accountID);
-console.table([accountID,accountEmail,accountPwd,accountCity]);
+console.table([accountID,accountEmail,accountPwd,accountCity,accountState]);
 
 
 
