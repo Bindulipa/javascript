99c1b9637d65cb2f11124c2c6705bda73e0d0193
Bindulipa <bindulipas@gmail.com>
2025-06-02 10:42:25 +0000

    datatype

---
+++ .devcontainer/01_basic/02_dataType.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js codeas newer version
+//alert(3+3) //we are using node js not browser
+
+console.log("hitesh")
+console.log(3+3)
+
+let name = "bindu"
+let age = 18
+let isLoggedIn = false
+
+/* primitive data types*/
+
+// number =>2 to the power 53
+// bigint -
+// string =>""
+// boolean=> true/false
+// null - standlone value [ representation of empty ]
+// undefined => when you don't assign value to the varible you have declared
+// symbol => unique
+
+
+console.log(typeof"Bindulipa");
+console.log(typeof null);
+console.log(typeof undefine)
+console.log(typeof age)
+console.log(typeof isLoggedIn)
+
+
+
+
+
+
+
